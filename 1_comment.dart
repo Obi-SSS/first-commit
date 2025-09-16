3c50c9c4340f2cd548b56b5c4ea0b6ff904f3d83
Obi-SSS <ayubicceanputra01@gmail.com>
2025-09-16 14:26:48 +0700

    add dart 1_comment

---
+++ 1_comment.dart
@@ -0,0 +1,23 @@
+// 1.single line coment menggunakan  //
+
+// 2. multi comment coment menggunakan /* ... */
+
+// 3. documention comment menggunakan /// atau /** ... */
+
+
+
+
+/**
+ *  ini adalan contoh[main] and [multipy]
+ * 
+ *  */
+void main(){
+  print('dart is fun!');
+
+  print("5 * 5 = ${multyply()}");
+}
+
+int multyply(){
+  return 5 * 5;
+
+}
