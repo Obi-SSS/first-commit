9ccb144702fde9f6d15cff6f07faa3ee8cf390eb
Obi-SSS <ayubicceanputra01@gmail.com>
2025-09-17 14:28:12 +0700

    belajar commit

---
+++ 3_type_data.dart
@@ -0,0 +1,54 @@
+ /**jenis data adalah ....
+  * 
+  *
+  *
+  */
+
+  /**
+   * int = interger - bilangan bulat
+   * 1, 2 ,11 dan sbg
+   */
+
+  /**
+   * dobule = bilangan desimal
+   * 3.14, -0.1, 0.2 =
+   */
+
+  /**
+   * num = gabunbgan dari interger dan dobule
+   * 3.14, 2, -2, 0, 3.14 
+   */
+
+  /**
+   * bool = bolean = true of false
+   */
+
+  /**
+   * string = "kumpulan karakter"
+   */
+
+  /**
+   * list = berupa daftar
+   * [1234567]
+   */
+
+  /**
+   * map = berupa key:value
+   * {"nama kota" : "jakarta", "ID" : 62}
+   */
+
+  /**
+   * diynamic = campuran/tipe apapun
+   */
+
+void main(List<String> args) {
+  string nama = "orang";
+  int age = 14;
+
+  var tinggi_badan;
+  tinggi_badan = 169;
+  tinggi badan = "seratius enam sembilan";
+
+  bool ismarried = false;
+  double beradbadan = 67.7;
+}
