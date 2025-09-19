0230a81df6fbc9b14f2a278185de9b4ee45ae081
Obi-SSS <ayubicceanputra01@gmail.com>
2025-09-19 10:30:49 +0700

    Belajar bolean

---
+++ 7_bolean.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  /**
+   *  simbol ! menandakan non / bukan
+   *  !=
+   */
+
+  /**
+   * tipe data bolean hanya ada 2 nilai
+   * truea dan false
+   */
+  var isTrue = true;
+
+  print(isTrue); //true
+  print(!isTrue); //false
+  print(false != true);
+}
