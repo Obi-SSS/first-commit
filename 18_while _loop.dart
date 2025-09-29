d2a04fa810011c76988662509238d43059c3315f
Obi-SSS <ayubicceanputra01@gmail.com>
2025-09-29 14:18:49 +0700

    while loop

---
+++ 18_while _loop.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  /**
+   * perulangan while dugunakan untuk menjalannkan a kode block
+   * selama kondisi block tersebut berninali true
+   * con digunakan untuk perulangan untuk perulangan dengan jumlah yang tidak
+   * scr pasti diketahui sejak awal
+   * penggunaan di awalki dnegan while(kondisi){kode yang di ulangi}
+   */
+  var i = 0;
+  while (i <= 10) ;
+  {
+    print('ini baris ke $i');
+    i++;
+  }
+  var angka = 10;
+  while (angka >= 0) {
+    print('ini bwabwabwa $angka');
+    angka--;
+  }
+}
